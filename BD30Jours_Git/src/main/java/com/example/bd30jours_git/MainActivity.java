70cb4a318395d65c95b66170747d76fae85d67a6
DimitriHeloin <dimitri.heloin@ingesup.com>
2014-02-05 15:43:44 +0100

    Avancement list-view layouts
    fragment fait
    rating star customisé

--- BD30Jours_Git/src/main/java/com/example/bd30jours_git/MainActivity.java
+++ BD30Jours_Git/src/main/java/com/example/bd30jours_git/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bd30jours_git;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -18,41 +19,15 @@ import android.widget.SimpleAdapter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
 
-    private ListView maListViewPerso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        maListViewPerso = (ListView) findViewById(R.id.listviewperso);
-        // getsupportfragmentmanager().findfragmentbyid(r.id.)
-
-        //Création de la ArrayList qui nous permettra de remplire la listView
-        ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
-
-        //On déclare la HashMap qui contiendra les informations pour un item
-        HashMap<String, String> map;
-
-        //Création d'une HashMap pour insérer les informations du premier item de notre listView
-        map = new HashMap<String, String>();
-        //on insère un élément titre que l'on récupérera dans le textView titre créé dans le fichier affichageitem.xml
-        map.put("titre_vue_unitaire", "Word");
-        //on insère un élément description que l'on récupérera dans le textView description créé dans le fichier affichageitem.xml
-        map.put("description_vue_unitaire", "Editeur de texte");
-        //on insère la référence à l'image (convertit en String car normalement c'est un int) que l'on récupérera dans l'imageView créé dans le fichier affichageitem.xml
-        map.put("imageButton_vue_unitaire", String.valueOf(R.drawable.previewalakon));
-        //enfin on ajoute cette hashMap dans la arrayList
-        listItem.add(map);
-
-        SimpleAdapter mSchedule = new SimpleAdapter (this.getBaseContext(), listItem, R.layout.bd_preview,
-                new String[] {"imageButton_vue_unitaire", "titre_vue_unitaire", "description_vue_unitaire"}, new int[] {R.id.imageButton_vue_unitaire, R.id.titre_vue_unitaire, R.id.description_vue_unitaire});
-
-
-        //maListViewPerso.setAdapter(mSchedule);
-
+        MainFragment fragment = (MainFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_list);
 
 
 
